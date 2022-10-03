5e457c60933e999832a6f7fe125b68a2631f4ea8
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-10-03 00:14:27 -0600

    create main component and home controller
    * update project title display
    * update root position

---
+++ app/javascript/components/_main.js.jsx
@@ -0,0 +1,9 @@
+const Main = ( props ) => {
+  return(
+    <div>
+      <h1>Pens are great!</h1>
+    </div>
+  )
+}
+
+export default Main
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
   namespace :api do
     namespace :v1 do
       resources :pens, only: [:index, :create, :destroy, :update]
     end
   end
-
-  root to: 'home#index'
-
 end
