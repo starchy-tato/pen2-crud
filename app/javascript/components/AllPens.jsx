ebc889136a6bb697c1a0c81124456ee5f996924d
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-10-03 23:47:35 -0600

    update AllPens component
    * add constructor, componentDidMount & pen list

--- app/javascript/components/AllPens.jsx
+++ app/javascript/components/AllPens.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
 class AllPens extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pens: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/pens.json')
+      .then((res) => {return res.json()})
+      .then((data) => {this.setState({ pens: data })});
+  }
 
   render() {
+    let pens = this.state.pens.map((pen) => {
+      return(
+        <div key={pen.id}>
+          <h1>{pen.name}</h1>
+          <p>{pen.description}</p>
+        </div>
+      )
+    })
+    
     return(
       <div>
-        <h1>List of pens</h1>
+        {pens}
       </div>
     )
   }
