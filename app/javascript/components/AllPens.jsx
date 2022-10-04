6b40b604d024bf76428af2b9e7df06a178689a75
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-10-03 20:02:21 -0600

    create AllPens component
    * add to Main component

---
+++ app/javascript/components/AllPens.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AllPens extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>List of pens</h1>
+      </div>
+    )
+  }
+}
+
+export default AllPens
