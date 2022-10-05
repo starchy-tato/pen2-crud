4340f6012dfba1a8dff652dd09b90b6de908d13d
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-10-04 21:03:45 -0600

    add onSubmit to form

--- app/javascript/components/NewItem.jsx
+++ app/javascript/components/NewItem.jsx
@@ -5,7 +5,14 @@ const NewPen = (props) => {
 
   return(
     // ref attribute tag to access certain DOM element - these grab value of inputs
-    <form>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      props.handleFormSubmit(
+        formFields.name.value,
+        formFields.description.value
+      );
+      event.target.reset()
+    }}>
       <input ref={input => formFields.name = input} placeholder='Enter item name'/>
       <input ref={input => formFields.description = input} placeholder='Enter a description' />
       <button>Submit</button>
