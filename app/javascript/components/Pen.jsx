2d82e3337a314aa2e0e796cf8f3a8e7c8d960e3c
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-10-05 21:16:32 -0600

    add state & editable attribute

--- app/javascript/components/Pen.jsx
+++ app/javascript/components/Pen.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 class Pen extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      editable: false
+    }
+  }
+
   render(){
     return(
       <div>
         <h1>{this.props.pen.name}</h1>
         <p>{this.props.pen.description}</p>
+        <button>{this.state.editable ? 'Submit' : 'Edit'}</button>
         <button
           onClick={() => this.props.handleDeletePen(this.props.pen.id)}
         >
