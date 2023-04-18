54c8daccf0b0939007db47ec59e8fb759336545f
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-04-17 23:53:10 -0600

    initial post create action spec
    * outline other end point specs

--- spec/api/pens_request_spec.rb
+++ spec/api/pens_request_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'GET #index', :type => :request do
-
   let!(:pen_one) { FactoryBot.create(:pen, name: 'Pilot Metropolitan', description: 'a classy fountain pen, with a fine nib' ) }
   let!(:pen_two) { FactoryBot.create(:pen, name: 'Bic Ballpoint', description: 'kinda basic') }
 
@@ -20,5 +19,37 @@ describe 'GET #index', :type => :request do
     expect(json_response[1]['name']).to eq 'Bic Ballpoint'
     expect(json_response[1]['description']).to eq pen_two.description
   end
+end
+
+describe 'POST #create', :type => :request do
+  before {get '/api/v1/pens'}
+
+  it 'returns a successful request' do
+    expect(response).to have_http_status(:success)
+  end
+
+  context 'it creates a new pen' do
+    let(:pen) { FactoryBot.create(:pen, name: 'Safari', description: 'vibrant colour') }
+
+    xit 'returns a new pen' do
+
+      json_response = JSON.parse(response.body)
+      #   expect a change in count
+      pp 'json res', json_response
+      expect(json_response).to change(Pen, :count).by(1)
+    end
+  end
+
+
+  xit 'returns the created pen' do
+
+  end
+end
+
+describe 'DELETE #destroy', :type => :request do
+
+end
+
+describe 'PUT #update', :type => :request do
 
 end
